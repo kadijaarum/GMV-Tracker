22e0376f61e1d42051dc6ad97870d50f393c7f46
Kadija Arum <kadijaarum@gmail.com>
2026-06-20 21:22:18 +0700

    Initial commit

---
+++ web-project/src/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+ 
+export const firebaseConfig = {
+  apiKey: "AIzaSyBJBlosK9mAE8bEGrnR29nMlxbLtCX25UE",
+  authDomain: "gmv-tracker-d3bed.firebaseapp.com",
+  projectId: "gmv-tracker-d3bed",
+  storageBucket: "gmv-tracker-d3bed.firebasestorage.app",
+  messagingSenderId: "920898498963",
+  appId: "1:920898498963:web:576fba5a83f7b4f3e8b866",
+};
+ 
+export const app = initializeApp(firebaseConfig);
